9bea0e84bbc3b55ddcdb300bdba5f9ad41ace0e0
bigstring-zer0 <147473025+bigstring-zer0@users.noreply.github.com>
2024-03-25 16:59:59 +0900

    Stack 구현.

---
+++ src/MyDataStructure/MyStack.java
@@ -0,0 +1,31 @@
+package MyDataStructure;
+
+import java.util.EmptyStackException;
+
+class MyStack<T> {
+    private final MyLinkedList<T> list = new MyLinkedList<>();
+
+    public void push(T element) {
+        list.add(element);
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        T element = list.get(list.size() - 1);
+        list.delete(list.size() - 1);
+        return element;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return list.get(list.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+}
